81d1c5781ce1c76f52411c66d8058218736116c0
allisonlee572 <121532627+allisonlee572@users.noreply.github.com>
2024-07-19 12:00:52 -0700

    Worked a bit more on enemy

---
+++ Bygone Stations/Assets/Scripts/Fighter.cs
@@ -0,0 +1,13 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// using RPG.Movement;
+// using RPG.Core;
+// using UnityEngine.InputSystem;
+
+// namespace RPG.Combat {
+//     public class Fighter: MonoBehaviour, IAction {
+
+//     }
+// }
