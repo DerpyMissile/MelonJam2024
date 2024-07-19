ee40592f6c07d4fd7d6723276c14283935aedf5d
allisonlee572 <121532627+allisonlee572@users.noreply.github.com>
2024-07-19 16:56:31 -0700

    Worked on enemy2 & enemy3

--- Bygone Stations/Assets/Scripts/Enemy2.cs
+++ Bygone Stations/Assets/Scripts/Enemy2.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+// using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class Enemy2 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+public class Enemy2 : MonoBehaviour {
+    [SerializeField] float speed; 
+    public float circleRadius; 
+    private Rigidbody2D EnemyRB;
+    public GameObject groundCheck;
+    public LayerMask groundLayer;
+    public bool facingRight; 
+    public bool isGrounded; 
+    Vector2 direction; 
+
+
+    void Start() {
+        EnemyRB = GetComponent<Rigidbody2D>(); 
+        facingRight = true; 
+        direction = new Vector2(90, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        transform.Translate(direction * speed * Time.deltaTime);
         
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, circleRadius, groundLayer); 
+        if (!isGrounded) {
+            Flip();
+        }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        transform.Rotate(new Vector3(0, 180, 0));
+        speed = -speed; 
+        direction = -direction; 
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.transform.position, circleRadius); 
     }
 }
