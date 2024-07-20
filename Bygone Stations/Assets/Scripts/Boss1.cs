9d6756f9a6353acdfff3b2462713a646b713552c
DerpyMissile <hmai015@ucr.edu>
2024-07-20 11:39:41 -0700

    main menu

---
+++ Bygone Stations/Assets/Scripts/Boss1.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1 : MonoBehaviour
+{
+    [SerializeField] float chaseDistance = 6f; 
+    [SerializeField] float jumpDistance = 3f; 
+    [SerializeField] float chaseSpeed = 0.5f; 
+    [SerializeField] float jumpSpeed = 3.5f; 
+
+    private Rigidbody2D EnemyRB;
+    private float health; 
+    
+
+    GameObject player;
+
+    private void Start() {
+        player = GameObject.FindWithTag("Player"); 
+        health = 1f; 
+        EnemyRB = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update() {
+        if (DistanceToPlayer() < chaseDistance) {
+            Vector2 direction = player.transform.position - transform.position;
+            if (direction.x > 0) {
+                direction = new Vector2(1, 0);
+            }
+            else {
+                direction = new Vector2(-1, 0);
+            }
+
+            if (DistanceToPlayer() > jumpDistance) {
+                transform.Translate(direction * chaseSpeed * Time.deltaTime);
+            }
+            else {
+                transform.Translate(direction * jumpSpeed * Time.deltaTime);
+            }
+        }
+    }
+    
+    private float DistanceToPlayer(){           
+        // Gives us distance between player and enemy 
+        return Vector2.Distance(player.transform.position, transform.position);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("OUCH!" + health); 
+        health -= 1; 
+        if (health == 0) {
+            Destroy(this.gameObject, 0.2f); 
+        }
+        else if(this.GetComponent<Transform>().position.x < other.gameObject.GetComponent<Transform>().position.x){
+            EnemyRB.velocity -= new Vector2(5, 1);
+        }
+        else {
+            EnemyRB.velocity += new Vector2(5, 1);
+        }
+    }
+}
---
+++ Bygone Stations/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startGame;
+    public Image screen;
+
+    public void Start(){
+        Color temp = screen.color;
+        temp.a = 1.0f;
+        screen.color = temp;
+        startGame.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick(){
+        StartCoroutine(switchScene());
+    }
+
+    IEnumerator switchScene()
+    {
+        yield return new WaitForSeconds(2);
+        // fade to black
+        float fadeDuration = 1.0f;
+        float elapsedTime = 0.0f;
+        Color temp = screen.color;
+
+        while (elapsedTime < fadeDuration)
+        {
+            yield return null;
+            elapsedTime += Time.deltaTime;
+            temp.a = Mathf.Lerp(1.0f, 0.0f, elapsedTime / fadeDuration);
+            screen.color = temp;
+        }
+
+        temp.a = 0.0f;
+        screen.color = temp;
+
+        // switch scene
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Station-1");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.UnloadSceneAsync("MainMenu");
+    }
+}
